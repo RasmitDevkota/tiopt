23a42b16281eae1a837228fe84c5dbae0222c2bd
Alexander-Feng <alexander.y.feng@gmail.com>
2025-03-29 14:52:18 -0700

    added electrostatics calcuation file to be called in c

---
+++ electrostatics.cpp
@@ -0,0 +1,22 @@
+#include "sparselizard.h"
+
+
+using namespace sl;
+
+extern "C" { 
+void electrostatic(char* meshfile, char* outputfile) {
+	int vol = 1, sur = 2;              // Disk volume and boundary as set in ’disk.geo’
+	mesh mymesh(meshfile);
+
+	field v("h1");                     // Nodal shape functions for the electric potential
+	v.setorder(vol, 2);                // Interpolation order 2 on the whole domain
+	v.setconstraint(sur, 1);           // Force 1 V on the disk boundary
+
+	formulation elec;                  // Electrostatics with 0 charge density
+	elec += integral(vol, -8.854e-12 * grad(dof(v)) * grad(tf(v)));
+
+	elec.solve();                      // Generate, solve and save solution to field v
+
+	(-grad(v)).write(vol, outputfile, 2); // Write the electric field to ParaView format
+}
+}
