4b42f8743566238b393fb4597f465a257ff5df5b
Rasmit Devkota <rdatch101@gmail.com>
2025-04-27 19:42:22 -0400

    converted all GMSH code to OpenCASCADE and made improvements/fixes

--- trap_geometry.c
+++ trap_geometry.c
@@ -9,17 +9,18 @@
 void generate_mesh(
 	struct Trap *trap
 ) {
-	const double lc = 1E-2;
+	const double lc = 1E-1;
 	for (int e = 0; e < trap->n_electrodes; e++)
 	{
 		printf("Generating mesh for electrode %d/%d\n", e+1, trap->n_electrodes);
 
 		int ierr;
 
-		int argc;
-		char** argv;
+		int argc = 0;
+		char** argv = NULL;
 		gmshInitialize(argc, argv, 1, 0, &ierr);
 
+		// Format unique electrode_id for model and mesh file
 		char electrode_id[11];
 		sprintf(
 			electrode_id,
@@ -28,6 +29,7 @@ void generate_mesh(
 			e
 		);
 
+		// Replace whitespace padding (default behavior of sprintf) with '0' padding
 		for (int p = 0; electrode_id[p] != '\0'; p++)
 		{
 			if (electrode_id[p] == ' ')
@@ -40,52 +42,112 @@ void generate_mesh(
 		int physgroup_vertices[(*trap->electrodes)[e].n_vertices];
 		for (int v = 0; v < (*trap->electrodes)[e].n_vertices; v++)
 		{
-			int vertex_v = gmshModelGeoAddPoint((*trap->electrodes)[e].vertices[v][0], (*trap->electrodes)[e].vertices[v][1], 0.0, lc, v+1, &ierr);
+			int vertex_v = gmshModelOccAddPoint(
+				(*trap->electrodes)[e].vertices[v][0], (*trap->electrodes)[e].vertices[v][1], (*trap->electrodes)[e].vertices[v][2],
+				lc,
+				v+1,
+				&ierr
+			);
 
 			physgroup_vertices[v] = vertex_v;
 		}
+		printf("gmshModelOccAddPoint Result: %d\n", ierr);
 
 		// 1D - Edges
 		int physgroup_edges[(*trap->electrodes)[e].n_edges];
 		for (int d = 0; d < (*trap->electrodes)[e].n_edges; d++)
 		{
-			int edge_e = gmshModelGeoAddLine((*trap->electrodes)[e].edges[d][0]+1, (*trap->electrodes)[e].edges[d][1]+1, d+1, &ierr);
+			int v_i = (*trap->electrodes)[e].edges[d][0];
+			int v_f = (*trap->electrodes)[e].edges[d][1];
 
-			physgroup_edges[e] = edge_e;
+			int edge_e = gmshModelOccAddLine(
+				physgroup_vertices[v_i], physgroup_vertices[v_f],
+				d+1,
+				&ierr
+			);
+
+			physgroup_edges[d] = edge_e;
 		}
+		printf("gmshModelOccAddLine Result: %d\n", ierr);
 
 		// 2D - Curve loops
-		// %TODO - make sure that the edge list has the appropriate order and signs
-		//       - this can either be a user expectation at input or enforced here
 		int curveloop_definition[(*trap->electrodes)[e].n_edges];
 		for (int d = 0; d < (*trap->electrodes)[e].n_edges; d++)
 		{
-			curveloop_definition[d] = d+1;
+			curveloop_definition[d] = physgroup_edges[d];
 		}
-		int curveloop = gmshModelGeoAddCurveLoop(curveloop_definition, sizeof(curveloop_definition)/sizeof(curveloop_definition[0]), e+1, 0, &ierr);
+		int curveloop = gmshModelOccAddCurveLoop(
+			curveloop_definition,
+			sizeof(curveloop_definition)/sizeof(curveloop_definition[0]),
+			2*e+1,
+			&ierr
+		);
+		printf("gmshModelOccAddCurveLoop Result: %d\n", ierr);
 
 		// 2D - Surfaces
-		const int surface_definition[] = { 1 };
-		int surface = gmshModelGeoAddPlaneSurface(surface_definition, sizeof(surface_definition)/sizeof(surface_definition[0]), e+1, &ierr);
+		const int surface_definition[1] = { curveloop };
+		int surface = gmshModelOccAddPlaneSurface(
+			surface_definition,
+			sizeof(surface_definition)/sizeof(surface_definition[0]),
+			2*e+2,
+			&ierr
+		);
 		int physgroup_surfaces[1] = { surface };
+		printf("gmshModelOccAddSurface Result: %d\n", ierr);
 
-		// // 3D - Cubes
-		// int box = gmshModelOccAddBox(0.0, 0.0, 0.0, 10.0, 10.0, 10.0, &ierr);
-		// // 3D - Spheres
-		// int sphere = gmshModelOccAddSphere(0.0, 0.0, 75.0, 70.0, 0, 2*M_PI, 0, M_PI, &ierr);
-		// int physgroup_volumes[1] = { sphere };
+		// 3D - Spheres
+		int sphere = gmshModelOccAddSphere(
+			0.0, 0.0, 75.0,
+			70.0,
+			e+1,
+			-0.5*M_PI, 0.5*M_PI, 2*M_PI,
+			&ierr
+		);
+		int physgroup_volumes[1] = { sphere };
+		printf("gmshModelOccAddSphere Result: %d\n", ierr);
 
 		// TODO - synchronization currently performed after every electrode, determine most efficient rate
-		gmshModelGeoSynchronize(&ierr);
-		// gmshModelOccSynchronize(&ierr);
+		gmshModelOccSynchronize(&ierr);
+		printf("gmshModelOccSynchronize Result: %d\n", ierr);
 
-		gmshModelAddPhysicalGroup(0, physgroup_vertices, sizeof(physgroup_vertices)/sizeof(physgroup_vertices[0]), 4*e+0, "vertices", &ierr);
-		gmshModelAddPhysicalGroup(1, physgroup_edges, sizeof(physgroup_edges)/sizeof(physgroup_edges[0]), 4*e+1, "edges", &ierr);
-		gmshModelAddPhysicalGroup(2, physgroup_surfaces, sizeof(physgroup_surfaces)/sizeof(physgroup_surfaces[0]), 4*e+2, "surfaces", &ierr);
-		// gmshModelAddPhysicalGroup(3, physgroup_volumes, sizeof(physgroup_volumes)/sizeof(physgroup_volumes[0]), 4*e+3, "volumes", &ierr);
+		// Add physical groups to model
+		gmshModelAddPhysicalGroup(
+			0,
+			physgroup_vertices,
+			sizeof(physgroup_vertices)/sizeof(physgroup_vertices[0]),
+			4*e+1,
+			"vertices",
+			&ierr
+		);
+		gmshModelAddPhysicalGroup(
+			1,
+			physgroup_edges,
+			sizeof(physgroup_edges)/sizeof(physgroup_edges[0]),
+			4*e+2,
+			"edges",
+			&ierr
+		);
+		gmshModelAddPhysicalGroup(
+			2,
+			physgroup_surfaces,
+			sizeof(physgroup_surfaces)/sizeof(physgroup_surfaces[0]),
+			4*e+3,
+			"surfaces",
+			&ierr
+		);
+		gmshModelAddPhysicalGroup(
+			3,
+			physgroup_volumes,
+			sizeof(physgroup_volumes)/sizeof(physgroup_volumes[0]),
+			4*e+4,
+			"volumes",
+			&ierr
+		);
 
-		gmshModelMeshGenerate(2, &ierr);
+		// Generate mesh
+		gmshModelMeshGenerate(3, &ierr);
 
+		// Save mesh to file (forcing msh file version 2.0 for Sparselizard compatibility)
 		gmshOptionSetNumber("Mesh.MshFileVersion", 2.0, &ierr);
 
 		sprintf((*trap->electrodes)[e].electrode_mesh_filename, "meshes/%s.msh", electrode_id);
